6d824d61524e0479f13566efafcef16902abd6bd
Virtus_Chae <ybd4731@gmail.com>
2024-06-18 18:34:50 +0900

    PaymentController 및 결제 페이지(payment.html) 추가

---
+++ src/main/java/com/nhnacademy/codequestweb/controller/payment/PaymentController.java
@@ -0,0 +1,12 @@
+package com.nhnacademy.codequestweb.controller.payment;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PaymentController {
+    @GetMapping("/payment")
+    public String payment() {
+        return "view/payment/payment";
+    }
+}
