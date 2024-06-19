c026157911a8ffc4f07220f570a2c98ce4790ba5
Virtus_Chae <ybd4731@gmail.com>
2024-06-19 15:23:56 +0900

    dummy data 포함하여 구현

---
+++ src/main/java/com/nhnacademy/codequestweb/temp/Coupon.java
@@ -0,0 +1,11 @@
+package com.nhnacademy.codequestweb.temp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Coupon {
+    Long id;
+    String name;
+}
