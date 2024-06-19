9493667c0d7330c470770b79954f2c485d1eb46f
Virtus_Chae <ybd4731@gmail.com>
2024-06-19 15:23:56 +0900

    dummy data 포함하여 구현

--- src/main/java/com/nhnacademy/codequestweb/controller/payment/PaymentController.java
+++ src/main/java/com/nhnacademy/codequestweb/controller/payment/PaymentController.java
@@ -1,13 +1,5 @@
 package com.nhnacademy.codequestweb.controller.payment;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
 public class PaymentController {
 
-    @GetMapping("/payment")
-    public String payment() {
-        return "view/payment/payment";
-    }
 }
