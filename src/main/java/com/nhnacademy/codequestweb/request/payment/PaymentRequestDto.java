923b33a7576e516d47db8909ff59711e7ad1ba5e
Virtus_Chae <ybd4731@gmail.com>
2024-06-21 08:05:27 +0900

    commit

--- src/main/java/com/nhnacademy/codequestweb/request/payment/PaymentRequestDto.java
+++ src/main/java/com/nhnacademy/codequestweb/request/payment/PaymentRequestDto.java
@@ -3,17 +3,14 @@ package com.nhnacademy.codequestweb.request.payment;
 import com.nhnacademy.codequestweb.temp.Coupon;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class PaymentRequestDto {
     @NotNull
-    private Long remainingPoints;   // 사용자가 보유하고 있는 포인트
     private Long pointToUse;        // 이번 결제에서 사용할 포인트
-    private List<Coupon> coupons;   // 사용자가 보유하고 있는 쿠폰들
     private Long couponId;          // 이번 결제에서 사용할 쿠폰의 아이디
-    private Long originalAmount;    // 원래 결제해야 하는 금액 (주문에서 받아 옴)
-    private Long finalAmount;       // 최종으로 결제해야 하는 값 (주문에서 받아 옴 * 쿠폰 - 포인트로 계산)
-    private Long expectedPoints;    // 최종으로 결제해야 하는 값 (포인트 정책과 곱해서 계산)
     private Long paymentMethodId;   // 결제 수단의 아이디
 }
