0f077a9f582ec570998115b96c6f10cbf4118045
jjeonmin <jms959310@gmail.com>
2024-06-20 10:55:08 +0900

    feat controller add

---
+++ src/main/java/com/nhnacademy/codequestweb/response/auth/coupon/CouponResponseDto.java
@@ -0,0 +1,8 @@
+package com.nhnacademy.codequestweb.response.auth.coupon;
+
+import com.nhnacademy.codequestweb.domain.Status;
+
+import java.time.LocalDateTime;
+
+public record CouponResponseDto(long couponId, CouponTypeResponseDto couponType, CouponPolicyResponseDto couponPolicy, LocalDateTime issuedDate, LocalDateTime expirationDate, LocalDateTime usedDate, Status status) {
+}
