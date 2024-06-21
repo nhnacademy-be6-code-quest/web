4d085885ed0c64331a9d97671919b3c56d804a54
jjeonmin <jms959310@gmail.com>
2024-06-21 10:02:16 +0900

    feat add viewcontroller

--- src/main/java/com/nhnacademy/codequestweb/service/coupon/CouponPolicyService.java
+++ src/main/java/com/nhnacademy/codequestweb/service/coupon/CouponPolicyService.java
@@ -1,12 +1,13 @@
 package com.nhnacademy.codequestweb.service.coupon;
 
 import com.nhnacademy.codequestweb.client.coupon.CouponPolicyClient;
+import com.nhnacademy.codequestweb.request.coupon.CouponPolicyRequestDto;
 import com.nhnacademy.codequestweb.response.auth.coupon.CouponPolicyResponseDto;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class CouponPolicyService {
@@ -14,8 +15,15 @@ public class CouponPolicyService {
     @Autowired
     private CouponPolicyClient couponPolicyClient;
 
-    public List<CouponPolicyResponseDto> getAllCouponPolicies(){
-        return couponPolicyClient.getAllCouponPolices();
+    public Page<CouponPolicyResponseDto> getAllCouponPolicies(Pageable pageable) {
+       return couponPolicyClient.getAllCouponPolices(pageable);
+    }
+
+    public CouponPolicyResponseDto getCouponPolicy(long couponPolicyId){
+        return couponPolicyClient.getCouponPolicy(couponPolicyId);
+    }
+    public ResponseEntity<CouponPolicyRequestDto> savePolicy(CouponPolicyRequestDto couponPolicyRequestDto){
+       return couponPolicyClient.savePolicy(couponPolicyRequestDto);
     }
 
 }
