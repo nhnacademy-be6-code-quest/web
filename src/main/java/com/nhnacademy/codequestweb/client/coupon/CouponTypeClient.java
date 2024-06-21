7ed71622b3015a7525272dd75a1462c1f1d2357d
jjeonmin <jms959310@gmail.com>
2024-06-21 10:02:16 +0900

    feat add viewcontroller

--- src/main/java/com/nhnacademy/codequestweb/client/coupon/CouponTypeClient.java
+++ src/main/java/com/nhnacademy/codequestweb/client/coupon/CouponTypeClient.java
@@ -9,6 +9,6 @@ import java.util.List;
 @FeignClient(name="couponTypeClient", url = "http://localhost:8006")
 public interface CouponTypeClient {
 
-    @GetMapping("/admin/type")
+    @GetMapping("/admin/coupon/type")
     List<CouponTypeResponseDto> findAllType();
 }
