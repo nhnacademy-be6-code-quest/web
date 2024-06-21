f2c4de9fdc998e911e7735af8622610bc9f30ec7
gihwanJang <90659997+gihwanJang@users.noreply.github.com>
2024-06-21 12:40:14 +0900

    feat : add mypage

--- src/main/java/com/nhnacademy/codequestweb/client/auth/AuthClient.java
+++ src/main/java/com/nhnacademy/codequestweb/client/auth/AuthClient.java
@@ -1,15 +1,20 @@
 package com.nhnacademy.codequestweb.client.auth;
 
-import com.nhnacademy.codequestweb.request.auth.ClientRegisterRequestDto;
-import com.nhnacademy.codequestweb.response.auth.ClientRegisterResponseDto;
-import jakarta.validation.Valid;
+import com.nhnacademy.codequestweb.request.auth.ClientLoginRequestDto;
+import com.nhnacademy.codequestweb.response.auth.ClientLoginResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "register", url = "http://localhost:8001")
+@FeignClient(name = "auth", url = "http://localhost:8001")
 public interface AuthClient {
-    @PostMapping("/api/client")
-    ResponseEntity<ClientRegisterResponseDto> register(@Valid @RequestBody ClientRegisterRequestDto clientRegisterRequestDto);
+    @PostMapping("/login")
+    ResponseEntity<ClientLoginResponseDto> login(@RequestBody ClientLoginRequestDto clientLoginRequestDto);
+    @PostMapping("/logout")
+    ResponseEntity<?> logout(@RequestHeader HttpHeaders headers);
+    @PostMapping("/reissue")
+    ResponseEntity<ClientLoginResponseDto> reissue(@RequestHeader(name = "refresh") String refresh);
 }
