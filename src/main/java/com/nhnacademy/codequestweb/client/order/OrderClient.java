5cfb024180199b0cecd9ee76493b9569abbbcecf
bakhuiwon326 <powerup326@naver.com>
2024-06-23 19:22:48 +0900

    feat: add delivery calendar in order page

--- src/main/java/com/nhnacademy/codequestweb/client/order/OrderClient.java
+++ src/main/java/com/nhnacademy/codequestweb/client/order/OrderClient.java
@@ -2,13 +2,13 @@ package com.nhnacademy.codequestweb.client.order;
 
 import com.nhnacademy.codequestweb.request.order.ClientOrderPostRequestDto;
 import com.nhnacademy.codequestweb.response.order.ClientOrderPostResponseDto;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "orderClient", url = "http://localhost:8008")
+@FeignClient(name = "order", url = "http://localhost:8008")
 public interface OrderClient {
     @PostMapping("/client/order")
     ResponseEntity<ClientOrderPostResponseDto> tryOrder(@RequestBody ClientOrderPostRequestDto orderRequestDto);
