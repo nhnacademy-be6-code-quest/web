efe5ed291a029b30e7967c62bfa8b341659ff9e8
Virtus_Chae <ybd4731@gmail.com>
2024-06-24 13:12:56 +0900

    style : 필요 없는 애너테이션 및 import 제거

--- src/main/java/com/nhnacademy/codequestweb/request/payment/OrderPaymentRequestDto.java
+++ src/main/java/com/nhnacademy/codequestweb/request/payment/OrderPaymentRequestDto.java
@@ -1,5 +1,10 @@
 package com.nhnacademy.codequestweb.request.payment;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class OrderPaymentRequestDto {
     Long orderId;
     Long totalPrice;
