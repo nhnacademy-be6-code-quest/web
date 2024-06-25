a2afb8c94ad53ca4f0d3b2affd496ae2feb00777
bakhuiwon326 <powerup326@naver.com>
2024-06-25 10:37:47 +0900

    refactor: add ObjectMapperConfig for LocalDateTime Deserialize

---
+++ src/main/java/com/nhnacademy/codequestweb/deserializer/DateToLocalDateTimeDeserializer.java
@@ -0,0 +1,22 @@
+package com.nhnacademy.codequestweb.deserializer;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateToLocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
+
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    @Override
+    public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JacksonException {
+        String date = jsonParser.getText();
+        return LocalDateTime.parse(date, DATE_FORMATTER).toLocalDate().atStartOfDay();
+
+    }
+}
