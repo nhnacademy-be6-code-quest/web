6536720fdd0832e4985b6032b5f246def08c2192
gihwanJang <90659997+gihwanJang@users.noreply.github.com>
2024-06-26 13:50:57 +0900

    feat : client phone management

---
+++ src/main/java/com/nhnacademy/codequestweb/request/mypage/ClientRegisterPhoneNumberRequestDto.java
@@ -0,0 +1,12 @@
+package com.nhnacademy.codequestweb.request.mypage;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ClientRegisterPhoneNumberRequestDto {
+    private String phoneNumber;
+}
