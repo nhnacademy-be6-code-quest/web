edc893738b78c6ed4e4c0e279809eba44e8a07f8
jjeonmin <jms959310@gmail.com>
2024-06-28 00:23:50 +0900

    feat: couponservice header => long clientId modify

--- src/main/java/com/nhnacademy/codequestweb/client/coupon/ClientCouponClient.java
+++ src/main/java/com/nhnacademy/codequestweb/client/coupon/ClientCouponClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name="clientCoupon", url="http://10.220.222.49:8001")
+@FeignClient(name="clientCoupon", url="http://localhost:8001")
 public interface ClientCouponClient {
 
     @GetMapping("/api/client/coupon-payment")
