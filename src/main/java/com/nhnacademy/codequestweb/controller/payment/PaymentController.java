7c68bd25261107b86b9070f7e4a980caaceaaaba
Virtus_Chae <ybd4731@gmail.com>
2024-06-30 17:32:49 +0900

    feat: 결제 페이지를 사용자에게 보여주기 위한 View 구성

--- src/main/java/com/nhnacademy/codequestweb/controller/payment/PaymentController.java
+++ src/main/java/com/nhnacademy/codequestweb/controller/payment/PaymentController.java
@@ -1,25 +1,43 @@
 package com.nhnacademy.codequestweb.controller.payment;
 
+import com.nhnacademy.codequestweb.response.payment.OrderPaymentResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
 public class PaymentController {
-    // 왜인지는 모르겠으나, https 가 아닌 http 로 해주어야 함.
-    @GetMapping("/client/order/{orderId}/payment")
-    public String savePayment(@PathVariable long orderId, Model model) {
+
+    /*
+        TODO: 주문에서 결제로 넘어와야 하는 값
+            - 구매자의 고유 아이디
+            - 회원 주문인지, 비회원 주문인지 식별할 수 있는 값
+            - 최종적으로 결제해야 하는 값 (order_total_amount, discount_amount_by_coupon, discount_amount_by_point)
+            - 결제한 상품 이름 1개 및 총 주문 개수 ex) "NHN Academy 에서 살아남기 외 2권"
+            - successUrl, failUrl
+            - 결제자 이메일
+            - 결제자 이름
+            - 결제자 휴대폰 번호 (주문자 휴대폰 번호가 아님)
+     */
+
+    @GetMapping("/client/order/payment")
+    public String savePayment(
+        @PathVariable long orderId, Model model,
+        @ModelAttribute OrderPaymentResponseDto orderPaymentResponseDto) {
+
         return "/view/payment/checkout";
     }
 
     // 사용자에게 결제와 관련된 정보를 입력 받습니다.
-    @PostMapping("client/order/{orderId}/payment")
-    public void savePayment(@PathVariable long orderId) {
+    @PostMapping("client/order/payment")
+    public void savePayment(/* @PathVariable long orderId */) {
     }
 }
--- src/main/java/com/nhnacademy/codequestweb/response/payment/OrderPaymentResponseDto.java
+++ src/main/java/com/nhnacademy/codequestweb/response/payment/OrderPaymentResponseDto.java
@@ -3,8 +3,20 @@ package com.nhnacademy.codequestweb.response.payment;
 import java.util.List;
 import lombok.Builder;
 
+/*
+    TODO: 주문에서 결제로 넘어와야 하는 값
+        - 구매자의 고유 아이디 (Auto Increment, Email, Phone Number 등 유추할 수 있는 값이면 안 됨.)
+        - 회원 주문인지, 비회원 주문인지 식별할 수 있는 값
+        - 최종적으로 결제해야 하는 값 (order_total_amount, discount_amount_by_coupon, discount_amount_by_point)
+        - 결제한 상품 이름 1개 및 총 주문 개수 ex) "NHN Academy 에서 살아남기 외 2권"
+        - successUrl, failUrl
+        - 결제자 이메일
+        - 결제자 이름
+        - 결제자 휴대폰 번호 (주문자 휴대폰 번호가 아님)
+ */
+
 @Builder
 public class OrderPaymentResponseDto {
-    long totalPrice;
+    // TODO: 구매자의 고유 아이디 고민해 보기 (아직 안 들어 감)
     List<ProductOrderDetailResponseDto> productOrderDetailDtoList;
 }
