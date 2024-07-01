89a7d4e6ff8f846e1729089df48c0c3658c5dc4a
Virtus_Chae <ybd4731@gmail.com>
2024-07-01 20:48:49 +0900

    feat: 테스트 데이터 넣어 결제 돌아가는 것 확인

--- src/main/java/com/nhnacademy/codequestweb/controller/payment/PaymentController.java
+++ src/main/java/com/nhnacademy/codequestweb/controller/payment/PaymentController.java
@@ -1,14 +1,16 @@
 package com.nhnacademy.codequestweb.controller.payment;
 
 import com.nhnacademy.codequestweb.response.payment.OrderPaymentResponseDto;
+import com.nhnacademy.codequestweb.response.payment.ProductOrderDetailResponseDto;
+import com.nhnacademy.codequestweb.service.payment.PaymentService;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -16,41 +18,64 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Slf4j
 public class PaymentController {
 
-    /*
-        TODO: 주문에서 결제로 넘어와야 하는 값
-            - 구매자의 고유 아이디
-            - 회원 주문인지, 비회원 주문인지 식별할 수 있는 값
-            - 최종적으로 결제해야 하는 값 (order_total_amount, discount_amount_by_coupon, discount_amount_by_point)
-            - 결제한 상품 이름 1개 및 총 주문 개수 ex) "NHN Academy 에서 살아남기 외 2권"
-            - successUrl, failUrl
-            - 결제자 이메일
-            - 결제자 이름
-            - 결제자 휴대폰 번호 (주문자 휴대폰 번호가 아님)
-     */
+    private final PaymentService paymentService;
 
     @GetMapping("/client/order/payment")
-    public String savePayment(
-        // TODO: 나중에 주석 풀기 @PathVariable long orderId,
-        Model model,
-        @ModelAttribute OrderPaymentResponseDto orderPaymentResponseDto) {
+    public String savePayment(/* TODO: 나중에 주석 풀기 @PathVariable long orderId,*/ Model model) {
 
-        long payAmount = 99999L;
-        model.addAttribute("payAmount", payAmount);
+        ProductOrderDetailResponseDto productOrderDetailResponseDto = ProductOrderDetailResponseDto.builder()
+            .productId(1L)
+            .quantity(3L)
+            .pricePerProduct(10000L)
+            .productName("10000 원 짜리 테스트 상품")
+            .build();
 
-        String tossOrderId = UUID.randomUUID().toString();
-        model.addAttribute("tossOrderId", tossOrderId);
+        ProductOrderDetailResponseDto productOrderDetailResponseDto2 = ProductOrderDetailResponseDto.builder()
+            .productId(2L)
+            .quantity(2L)
+            .pricePerProduct(20000L)
+            .productName("20000 원 짜리 테스트 상품")
+            .build();
 
-        String orderName = "초코파이 외 10건";
-        model.addAttribute("orderName", orderName);
+        ProductOrderDetailResponseDto productOrderDetailResponseDto3 = ProductOrderDetailResponseDto.builder()
+            .productId(3L)
+            .quantity(1L)
+            .pricePerProduct(30000L)
+            .productName("30000 원 짜리 테스트 상품")
+            .build();
 
+        List<ProductOrderDetailResponseDto> productOrderDetailResponseDtoList = new ArrayList<>();
+        productOrderDetailResponseDtoList.add(productOrderDetailResponseDto);
+        productOrderDetailResponseDtoList.add(productOrderDetailResponseDto2);
+        productOrderDetailResponseDtoList.add(productOrderDetailResponseDto3);
+
+//        OrderPaymentResponseDto orderPaymentResponseDto = paymentService.findOrderPaymentResponseDtoByOrderId(orderId);
+        OrderPaymentResponseDto orderPaymentResponseDto = OrderPaymentResponseDto.builder()
+            .orderTotalAmount(100000L)
+            .discountAmountByCoupon(20000L)
+            .discountAmountByPoint(20000L)
+            .tossOrderId(UUID.randomUUID().toString())
+            .productOrderDetailResponseDtoList(productOrderDetailResponseDtoList)
+            .build();
+
+        model.addAttribute("payAmount", orderPaymentResponseDto.getOrderTotalAmount()
+            - orderPaymentResponseDto.getDiscountAmountByCoupon()
+            - orderPaymentResponseDto.getDiscountAmountByCoupon());
+
+        model.addAttribute("tossOrderId", orderPaymentResponseDto.getTossOrderId());
+
+        model.addAttribute("orderName",
+            orderPaymentResponseDto.getProductOrderDetailResponseDtoList().getFirst()
+                .getProductName() + " 외 " + (
+                orderPaymentResponseDto.getProductOrderDetailResponseDtoList().size() - 1) + "건");
+
+//        String customerName = paymentService.getCustomerNameByOrderId(orderId);
         String customerName = "김채호";
         model.addAttribute("customerName", customerName);
 
-        String successUrl = "https://localhost:8080/client/order/payment/success";
-        model.addAttribute("successUrl", successUrl);
+        model.addAttribute("successUrl", "https://localhost:8080/client/order/payment/success");
 
-        String failUrl = "https://localhost:8080/client/order/payment/fail";
-        model.addAttribute("failUrl", failUrl);
+        model.addAttribute("failUrl", "https://localhost:8080/client/order/payment/fail");
 
         return "/view/payment/tossPage";
     }
@@ -65,8 +90,9 @@ public class PaymentController {
         return "view/payment/failed";
     }
 
-//    // 사용자에게 결제와 관련된 정보를 입력 받습니다.
-//    @PostMapping("client/order/payment")
-//    public void savePayment(/* TODO: 나중에 주석 풀기 @PathVariable long orderId */) {
-//    }
+    // 사용자에게 결제와 관련된 정보를 입력 받습니다.
+    @PostMapping("client/order/payment")
+    public void savePayment(/* TODO: 나중에 주석 풀기 @PathVariable long orderId */) {
+
+    }
 }
