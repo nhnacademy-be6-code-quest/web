7111450b3b6f15f85809de4f6b8bd2eb6cfcf6cb
Virtus_Chae <ybd4731@gmail.com>
2024-07-02 22:45:19 +0900

    feat: OrderService 에서 받아 오기 위한 서비스 메서드 추가

--- src/main/java/com/nhnacademy/codequestweb/service/payment/PaymentService.java
+++ src/main/java/com/nhnacademy/codequestweb/service/payment/PaymentService.java
@@ -1,7 +1,9 @@
 package com.nhnacademy.codequestweb.service.payment;
 
 import com.nhnacademy.codequestweb.client.payment.PaymentClient;
-import com.nhnacademy.codequestweb.request.payment.PaymentRequestDto;
+import com.nhnacademy.codequestweb.request.payment.PaymentOrderRequestDto;
+import com.nhnacademy.codequestweb.request.payment.PaymentOrderValidationRequestDto;
+import com.nhnacademy.codequestweb.response.payment.PaymentResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +13,16 @@ public class PaymentService {
 
     private final PaymentClient paymentClient;
 
-    public void savePayment(long orderId, PaymentRequestDto paymentRequestDto) {
-        paymentClient.savePayment(orderId, paymentRequestDto);
+    public void savePayment(long orderId, PaymentResponseDto paymentResponseDto) {
+        paymentClient.savePayment(orderId, paymentResponseDto);
+    }
+
+    public PaymentOrderRequestDto findOrderPaymentResponseDtoByOrderId(long orderId) {
+        return paymentClient.findOrderPaymentResponseDtoByOrderId(orderId);
+    }
+
+    public PaymentOrderValidationRequestDto findOrderTossPaymentResponseDtoByOrderId(long orderId) {
+        return paymentClient.findOrderTossPaymentResponseDtoByOrderId(orderId);
     }
 
     // PaymentId 로 Payment 관련 정보 얻기
