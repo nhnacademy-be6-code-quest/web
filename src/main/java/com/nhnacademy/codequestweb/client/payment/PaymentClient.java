362c77e331fa7e594b51cc40a09e5d9e90c4ecb6
Virtus_Chae <ybd4731@gmail.com>
2024-07-02 22:46:36 +0900

    feat: OrderService 에서 PaymentService 로 가져오기 위한 메서드 추가 - Dto 또한 수정했습니다.

--- src/main/java/com/nhnacademy/codequestweb/client/payment/PaymentClient.java
+++ src/main/java/com/nhnacademy/codequestweb/client/payment/PaymentClient.java
@@ -1,6 +1,7 @@
 package com.nhnacademy.codequestweb.client.payment;
 
-import com.nhnacademy.codequestweb.request.payment.PaymentRequestDto;
+import com.nhnacademy.codequestweb.request.payment.PaymentOrderRequestDto;
+import com.nhnacademy.codequestweb.request.payment.PaymentOrderValidationRequestDto;
 import com.nhnacademy.codequestweb.response.payment.PaymentResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
@@ -13,9 +14,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 public interface PaymentClient {
 
     @PostMapping("{orderId}/payment")
-    void savePayment(@PathVariable long orderId, @RequestBody PaymentRequestDto paymentRequestDto);
+    void savePayment(@PathVariable long orderId, @RequestBody PaymentResponseDto paymentResponseDto);
 
     @GetMapping("{orderId}/payment/{paymentId}")
     ResponseEntity<PaymentResponseDto> findByPaymentId(@PathVariable Long paymentId,
         @PathVariable long orderId);
+
+    @GetMapping("{orderId}")
+    PaymentOrderRequestDto findOrderPaymentResponseDtoByOrderId(@PathVariable long orderId);
+
+    @GetMapping("{orderId}")
+    PaymentOrderValidationRequestDto findOrderTossPaymentResponseDtoByOrderId(@PathVariable long orderId);
 }
