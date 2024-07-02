968860f087073daccd22b0e5f666d24518c9d448
Virtus_Chae <ybd4731@gmail.com>
2024-07-02 22:43:47 +0900

    feat: 결제에 저장하기 위해 토스 페이먼츠에서 받아오기 위한 Dto

--- src/main/java/com/nhnacademy/codequestweb/response/payment/PaymentResponseDto.java
+++ src/main/java/com/nhnacademy/codequestweb/response/payment/PaymentResponseDto.java
@@ -1,14 +1,28 @@
 package com.nhnacademy.codequestweb.response.payment;
 
-import java.time.LocalDateTime;
-import lombok.Data;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
 
-@Data
+/**
+ * 결제에 저장하기 위해 토스 페이먼츠에서 받아 오는 값들입니다.
+ * orderId 는 토스 페이먼츠에서 받아 오지 않습니다.
+ * @author 김채호
+ * @version 1.0
+ */
+@Builder
+@Getter
 public class PaymentResponseDto {
-    private Long paymentId;
-    private Long orderId;
-    private LocalDateTime paymentDate;
-    private Long clientDeliveryAddressId;
-    private PaymentMethodResponseDto paymentMethod;
-    private Long couponId;
+
+    @NotNull
+    long orderId;
+
+    @NotNull
+    long payAmount;
+
+    @NotNull
+    String paymentMethodName;
+
+    @NotNull
+    String tossPaymentKey;
 }
