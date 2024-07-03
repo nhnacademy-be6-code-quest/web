7a2b8d89b5e33b867e492d86a8f74078d8859a24
Virtus_Chae <ybd4731@gmail.com>
2024-07-03 23:14:28 +0900

    feat: 구현하지 않은 메서드 제거

--- src/main/java/com/nhnacademy/codequestweb/client/payment/PaymentClient.java
+++ src/main/java/com/nhnacademy/codequestweb/client/payment/PaymentClient.java
@@ -1,11 +1,8 @@
 package com.nhnacademy.codequestweb.client.payment;
 
-import com.nhnacademy.codequestweb.request.payment.PaymentOrderRequestDto;
-import com.nhnacademy.codequestweb.request.payment.PaymentOrderValidationRequestDto;
 import com.nhnacademy.codequestweb.response.payment.PaymentResponseDto;
+import com.nhnacademy.codequestweb.response.payment.TossPaymentsResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,15 +11,5 @@ import org.springframework.web.bind.annotation.RequestBody;
 public interface PaymentClient {
 
     @PostMapping("{orderId}/payment")
-    void savePayment(@PathVariable long orderId, @RequestBody PaymentResponseDto paymentResponseDto);
-
-    @GetMapping("{orderId}/payment/{paymentId}")
-    ResponseEntity<PaymentResponseDto> findByPaymentId(@PathVariable Long paymentId,
-        @PathVariable long orderId);
-
-    @GetMapping("{orderId}")
-    PaymentOrderRequestDto findOrderPaymentResponseDtoByOrderId(@PathVariable long orderId);
-
-    @GetMapping("{orderId}")
-    PaymentOrderValidationRequestDto findOrderTossPaymentResponseDtoByOrderId(@PathVariable long orderId);
+    void savePayment(@PathVariable long orderId, @RequestBody TossPaymentsResponseDto tossPaymentsResponseDto);
 }
