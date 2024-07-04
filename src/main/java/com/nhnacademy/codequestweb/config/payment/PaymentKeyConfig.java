c58dda45cfd962e078a9b305f967f846f0679b42
Virtus_Chae <ybd4731@gmail.com>
2024-07-04 17:00:41 +0900

    test condition(dev) 에서는 secretKey 접근 못하도록 변경 (CI 성공 위해) - NHN Cloud 에 IP 가 등록되어 있지 않으면 secretKey를 가져올 수 없음 - 그에 따라 CI 환경에서는 SecretKey를 Bean 에 등록하지 못함 - 그에 따라 CI 지속적으로 실패하는 현상 발견 - 일단은 prod 환경에서만 Bean 등록 되게 조치했음.

--- src/main/java/com/nhnacademy/codequestweb/config/payment/PaymentKeyConfig.java
+++ src/main/java/com/nhnacademy/codequestweb/config/payment/PaymentKeyConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONObject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 /**
  * PaymentKey 를 받아 오는 Bean 을 등록하는 파일입니다. resources 하위 디렉터리에 key 디렉터리를 만들고 paymentKey.txt 를 넣어 주시면
@@ -16,12 +17,14 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @RequiredArgsConstructor
+//@Profile({"dev", "prod"})
+@Profile("prod")
 public class PaymentKeyConfig {
 
     private final NhnKeyManagerClient nhnKeyManagerClient;
 
     @Bean
-    public String secretKey() {
+    public String secretKey() { // 테스트 환경에서 돌아가지 않게...
         JSONObject jsonObject = nhnKeyManagerClient.getTossSecretKey();
         Map<String, Object> responseMap = (Map<String, Object>) jsonObject;
         Map<String, Object> bodyMap = (Map<String, Object>) responseMap.get("body");
