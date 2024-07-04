075db9c12b4836dc9215d85689d4afd1407bee7e
Virtus_Chae <ybd4731@gmail.com>
2024-07-04 13:41:31 +0900

    docs: JavaDoc 추가

--- src/main/java/com/nhnacademy/codequestweb/client/payment/PaymentClient.java
+++ src/main/java/com/nhnacademy/codequestweb/client/payment/PaymentClient.java
@@ -1,15 +1,21 @@
 package com.nhnacademy.codequestweb.client.payment;
 
-import com.nhnacademy.codequestweb.response.payment.PaymentResponseDto;
 import com.nhnacademy.codequestweb.response.payment.TossPaymentsResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+/**
+ * OrderPaymentRefund API 로 인도하는 FeignClient 파일입니다.
+ *
+ * @author 김채호
+ * @version 1.0
+ */
 @FeignClient(name = "paymentClient", url = "http://localhost:8001/api/client/order")
 public interface PaymentClient {
 
     @PostMapping("{orderId}/payment")
-    void savePayment(@PathVariable long orderId, @RequestBody TossPaymentsResponseDto tossPaymentsResponseDto);
+    void savePayment(@PathVariable long orderId,
+        @RequestBody TossPaymentsResponseDto tossPaymentsResponseDto);
 }
