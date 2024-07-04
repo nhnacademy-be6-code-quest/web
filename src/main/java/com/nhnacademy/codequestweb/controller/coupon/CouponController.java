194efd253d5988204c900421e68227f6e5e051a8
jjeonmin <jms959310@gmail.com>
2024-07-04 12:40:26 +0900

    feat: coupon controller 추가

--- src/main/java/com/nhnacademy/codequestweb/controller/coupon/CouponController.java
+++ src/main/java/com/nhnacademy/codequestweb/controller/coupon/CouponController.java
@@ -1,28 +1,22 @@
 package com.nhnacademy.codequestweb.controller.coupon;
 
-import com.nhnacademy.codequestweb.client.auth.UserClient;
 import com.nhnacademy.codequestweb.domain.Status;
 import com.nhnacademy.codequestweb.request.coupon.CouponRequestDto;
-import com.nhnacademy.codequestweb.response.coupon.CouponResponseDto;
+import com.nhnacademy.codequestweb.response.coupon.ClientCouponPaymentResponseDto;
 import com.nhnacademy.codequestweb.response.coupon.CouponTypeResponseDto;
-import com.nhnacademy.codequestweb.service.coupon.CouponPolicyService;
+import com.nhnacademy.codequestweb.service.coupon.ClientCouponService;
 import com.nhnacademy.codequestweb.service.coupon.CouponService;
 import com.nhnacademy.codequestweb.service.coupon.CouponTypeService;
-import com.nhnacademy.codequestweb.response.coupon.ClientCouponPaymentResponseDto;
-import com.nhnacademy.codequestweb.service.coupon.ClientCouponService;
 import com.nhnacademy.codequestweb.utils.CookieUtils;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -38,31 +32,24 @@ public class CouponController {
     @Autowired
     private ClientCouponService clientCouponService;
 
-    @GetMapping("/api/client/coupon")
-    public String viewCoupon(Model model,HttpServletRequest req){
+
+    @GetMapping("/processUserSelection")
+    public String view(HttpServletRequest req,
+            Model model, @RequestParam(defaultValue = "10") int page, @RequestParam(defaultValue = "0") int size){
+
+
+
+
         HttpHeaders headers = new HttpHeaders();
         headers.set("access", CookieUtils.getCookieValue(req, "access"));
         headers.set("refresh", CookieUtils.getCookieValue(req, "refresh"));
-        List<CouponResponseDto> couponList = couponService.findClientCoupon(headers);
-        model.addAttribute("couponList",couponList);
-        return "/view/coupon/client_coupon_view";
-    }
-
-    @GetMapping("/processUserSelection")
-    public String view(
-            Model model, @RequestParam(defaultValue = "10") int page, @RequestParam(defaultValue = "0") int size){
-
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.set("X-User-Id","1");
-        httpHeaders.set("X-User-Role","ROLE_ADMIN");
-
-        Page<ClientCouponPaymentResponseDto> coupons = clientCouponService.getClient(httpHeaders, size, page);
+        Page<ClientCouponPaymentResponseDto> coupons = clientCouponService.getClient(headers, size, page);
         model.addAttribute("couponPayments",coupons);
         return "/view/coupon/coupon_client";
     }
 
     @GetMapping("/api/coupon/register/{couponPolicyId}")
-    public String saveCouponView(Model model, @PathVariable long couponPolicyId, HttpSession httpSession){
+    public String saveCouponView(Model model, @PathVariable long couponPolicyId){
         List<CouponTypeResponseDto> couponTypes = couponTypeService.getAllCouponTypes();
 
         List<Status> statuses = List.of(Status.AVAILABLE, Status.USED,Status.UNAVAILABLE);
@@ -70,6 +57,7 @@ public class CouponController {
         model.addAttribute("status",statuses);
         model.addAttribute("couponPolicyId",couponPolicyId);
         return "/view/coupon/admin_coupon_register";
+        //TODO 주소바꾸기
     }
 
     @PostMapping("/api/coupon/register/{couponPolicyId}")
