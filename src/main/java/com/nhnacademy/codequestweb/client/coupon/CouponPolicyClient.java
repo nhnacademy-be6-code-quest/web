0718a0783c8faed7b3003afbb0a7eef31ff39bca
jjeonmin <jms959310@gmail.com>
2024-07-04 12:40:56 +0900

    feat: coupon client 추가

--- src/main/java/com/nhnacademy/codequestweb/client/coupon/CouponPolicyClient.java
+++ src/main/java/com/nhnacademy/codequestweb/client/coupon/CouponPolicyClient.java
@@ -1,21 +1,23 @@
 package com.nhnacademy.codequestweb.client.coupon;
 
-import com.nhnacademy.codequestweb.request.coupon.CouponPolicyRequestDto;
-import com.nhnacademy.codequestweb.response.coupon.CouponPolicyResponseDto;
+import com.nhnacademy.codequestweb.request.coupon.CouponPolicyRegisterRequestDto;
+import com.nhnacademy.codequestweb.response.coupon.CouponPolicyListResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "couponPolicyClient", url="http://localhost:8001")
 public interface CouponPolicyClient {
 
     @GetMapping("/api/coupon/policy")
-    Page<CouponPolicyResponseDto> getAllCouponPolices(Pageable pageable);
+    ResponseEntity<Page<CouponPolicyListResponseDto>> getAllCouponPolices(@RequestParam int page, @RequestParam int size);
 
     @PostMapping("/api/coupon/policy/register")
-    ResponseEntity<CouponPolicyRequestDto> savePolicy(@RequestBody CouponPolicyRequestDto couponPolicyRequestDto);
+    ResponseEntity<CouponPolicyRegisterRequestDto> savePolicy(@RequestBody CouponPolicyRegisterRequestDto couponPolicyRegisterRequestDto);
 }
+
+
