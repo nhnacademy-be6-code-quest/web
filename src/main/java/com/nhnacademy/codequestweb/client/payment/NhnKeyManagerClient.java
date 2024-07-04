ffde0d38419e083b08992e81d206dfc8a07afc02
Virtus_Chae <ybd4731@gmail.com>
2024-07-04 23:53:44 +0900

    feat: JavaDoc 추가, 불필요한 Dto 제거

--- src/main/java/com/nhnacademy/codequestweb/client/payment/NhnKeyManagerClient.java
+++ src/main/java/com/nhnacademy/codequestweb/client/payment/NhnKeyManagerClient.java
@@ -4,9 +4,25 @@ import org.json.simple.JSONObject;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * NHN KeyManager 에서 Toss Payments 에 사용 될 secretKey 를 받아 오기 위해 외부 API 를 호출하는 @FeignClient 클래스입니다.
+ * NHN KeyManager 에 IP 가 등록 되어 있지 않다면 값을 가져올 수 없습니다. Toss Payments 에서 승인을 받지 못했다면, NHN KeyManager 에
+ * IP 등록이 되어있지 않거나 랜선 연결이 되어있지 않을 수 있습니다.
+ *
+ * @author 김채호
+ * @version 1.0
+ */
 @FeignClient(name = "nhnKeyManagerClient", url = "https://api-keymanager.nhncloudservice.com/keymanager/v1.0/appkey")
 public interface NhnKeyManagerClient {
 
-    @GetMapping("/2SxwmBzUfnqJaA2A/secrets/2b7a4c66d9cd42079e99a88283416515")
+    String APP_KEY = "${nhn.keymanager.appkey}";
+    String KEY_ID = "${nhn.keymanager.keyid}";
+
+    /**
+     * NHN KeyManager 에서 Toss Payments 에 사용될 secretKey 를 가져오는 메서드입니다.
+     *
+     * @return Toss Payments 에 사용될 secretKey
+     */
+    @GetMapping("/" + APP_KEY + "/secrets/" + KEY_ID)
     JSONObject getTossSecretKey();
 }
