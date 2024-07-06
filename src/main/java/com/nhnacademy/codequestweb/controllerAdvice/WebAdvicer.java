82d7641b255bbde8f1349b0617c3aa206fd07c33
gihwanJang <90659997+gihwanJang@users.noreply.github.com>
2024-07-07 07:46:08 +0900

    feat : update view

--- src/main/java/com/nhnacademy/codequestweb/controllerAdvice/WebAdvicer.java
+++ src/main/java/com/nhnacademy/codequestweb/controllerAdvice/WebAdvicer.java
@@ -2,13 +2,24 @@ package com.nhnacademy.codequestweb.controllerAdvice;
 
 
 import feign.FeignException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class WebAdvicer {
     @ExceptionHandler(FeignException.Unauthorized.class)
-    public String handleBadRequest(FeignException.Unauthorized ex) {
+    public String handleBadRequest(FeignException.Unauthorized ex, HttpServletResponse response) {
+        Cookie access = new Cookie("access", null);
+        Cookie refresh = new Cookie("refresh", null);
+        access.setMaxAge(0);
+        refresh.setMaxAge(0);
+        access.setPath("/");
+        refresh.setPath("/");
+        response.addCookie(access);
+        response.addCookie(refresh);
         return "redirect:/auth";
     }
 }
