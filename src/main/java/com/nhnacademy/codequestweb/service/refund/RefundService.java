9b3cb170bb4c5228d175c1c71680e0d70d4fae44
Virtus_Chae <ybd4731@gmail.com>
2024-07-09 21:39:55 +0900

    feat: 환불 관련 로직 추가

--- src/main/java/com/nhnacademy/codequestweb/service/refund/RefundService.java
+++ src/main/java/com/nhnacademy/codequestweb/service/refund/RefundService.java
@@ -1,7 +1,9 @@
 package com.nhnacademy.codequestweb.service.refund;
 
 import com.nhnacademy.codequestweb.client.refund.RefundClient;
-import com.nhnacademy.codequestweb.request.refund.RefundOrderRequestDto;
+import com.nhnacademy.codequestweb.client.refund.RefundOrderClient;
+import com.nhnacademy.codequestweb.controller.refund.RefundPolicyRequestDto;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +11,31 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class RefundService {
 
+//    private final RefundClient refundClient;
+    private final RefundOrderClient refundOrderClient;
     private final RefundClient refundClient;
 
-    public RefundOrderRequestDto findRefundOrderRequestDtoByOrderId(long orderId) {
-        return refundClient.findRefundOrderRequestDtoByOrderId(orderId);
+//    public RefundOrderRequestDto findRefundOrderRequestDtoByOrderId(long orderId) {
+//        return refundClient.findRefundOrderRequestDtoByOrderId(orderId);
+//    }
+//
+//    public String findOrderStatusByOrderId(Long orderId) {
+//        return refundClient.findOrderStatusByOrderId(orderId);
+//    }
+
+    public String getOrderStatus(long orderId) {
+        return refundOrderClient.findOrderStatusByOrderId(orderId);
+    }
+
+    public boolean isRefund(String orderStatus) {
+        return orderStatus.equals("배송중") || orderStatus.equals("배송완료");
+    }
+
+    public boolean isCancel(String orderStatus) {
+        return orderStatus.equals("결제대기") || orderStatus.equals("결제완료");
+    }
+
+    public List<RefundPolicyRequestDto> findAllRefundPolicyRequestDtoList() {
+        return refundClient.findAllRefundPolicyRequestDtoList();
     }
 }
