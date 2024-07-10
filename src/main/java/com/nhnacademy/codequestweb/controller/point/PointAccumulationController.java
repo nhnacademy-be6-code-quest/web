6d3f857c061f8ae10c6330c80bef1cec57431c10
jjeonmin <jms959310@gmail.com>
2024-07-10 15:54:06 +0900

    feat: 포인트 컨트롤러 추가

---
+++ src/main/java/com/nhnacademy/codequestweb/controller/point/PointAccumulationController.java
@@ -0,0 +1,45 @@
+package com.nhnacademy.codequestweb.controller.point;
+
+import com.nhnacademy.codequestweb.response.coupon.CouponMyPageCouponResponseDto;
+import com.nhnacademy.codequestweb.response.point.PointAccumulationAdminPageResponseDto;
+import com.nhnacademy.codequestweb.response.point.PointAccumulationMyPageResponseDto;
+import com.nhnacademy.codequestweb.service.point.PointAccumulationService;
+import com.nhnacademy.codequestweb.utils.CookieUtils;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+public class PointAccumulationController {
+
+    private final PointAccumulationService pointAccumulationService;
+
+    @GetMapping("/mypage/point/reward")
+    public String myPagePoint (HttpServletRequest req, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size){
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("access", CookieUtils.getCookieValue(req, "access"));
+
+        req.setAttribute("view", "mypage");
+        req.setAttribute("mypage", "points");
+        Page<PointAccumulationMyPageResponseDto> dto = pointAccumulationService.clientPoint(headers, page, size);
+        req.setAttribute("points", dto);
+        return "index";
+    }
+
+    @GetMapping("/admin/point/reward")
+    public String adminPagePoint (HttpServletRequest req, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size){
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("access", CookieUtils.getCookieValue(req, "access"));
+
+        req.setAttribute("view", "adminPage");
+        req.setAttribute("adminPage", "points");
+        Page<PointAccumulationAdminPageResponseDto> dto = pointAccumulationService.userPoint(headers, page, size);
+        req.setAttribute("points", dto);
+        return "index";
+    }
+}
