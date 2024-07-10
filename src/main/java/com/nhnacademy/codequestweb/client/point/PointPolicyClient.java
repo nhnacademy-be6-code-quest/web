ce759c8f1ef5d353fe9360e73c48645c7b299306
jjeonmin <jms959310@gmail.com>
2024-07-10 15:35:30 +0900

    feat: 포인트 서비스 추가

--- src/main/java/com/nhnacademy/codequestweb/client/point/PointPolicyClient.java
+++ src/main/java/com/nhnacademy/codequestweb/client/point/PointPolicyClient.java
@@ -3,23 +3,26 @@ import com.nhnacademy.codequestweb.request.point.PointPolicyRegisterRequestDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.data.domain.Page;
 import com.nhnacademy.codequestweb.response.point.PointPolicyAdminListResponseDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name="pointPolicy", url="http://localhost:8001")
 public interface PointPolicyClient {
 
     @PostMapping("/api/point/policy/register")
-    ResponseEntity<PointPolicyRegisterRequestDto> savePointPolicy(@RequestBody PointPolicyRegisterRequestDto pointPolicyRegisterRequestDto);
+    ResponseEntity<PointPolicyRegisterRequestDto> savePointPolicy(@RequestHeader HttpHeaders headers, @RequestBody PointPolicyRegisterRequestDto pointPolicyRegisterRequestDto);
 
     @GetMapping("/api/point/policy")
-    ResponseEntity<Page<PointPolicyAdminListResponseDto>> findAllPointPolicy(@RequestParam int page, @RequestParam int size);
+    ResponseEntity<Page<PointPolicyAdminListResponseDto>> findAllPointPolicy(@RequestHeader
+        HttpHeaders headers, @RequestParam int page, @RequestParam int size);
 
     @DeleteMapping("/api/point/policy/{pointPolicyId}")
-    ResponseEntity<String> deletePointPolicy(@PathVariable long pointPolicyId);
+    ResponseEntity<String> deletePointPolicy(@RequestHeader HttpHeaders headers, @PathVariable long pointPolicyId);
 }
