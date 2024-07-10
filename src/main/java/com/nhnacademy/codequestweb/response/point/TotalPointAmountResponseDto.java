eddd78f7fa3a8ee37b1cca3ad9b3499afa2d2f9f
jjeonmin <jms959310@gmail.com>
2024-07-10 20:20:34 +0900

    feat: 회원 포인트 뷰 추가 사이드바 url 추가

---
+++ src/main/java/com/nhnacademy/codequestweb/response/point/TotalPointAmountResponseDto.java
@@ -0,0 +1,12 @@
+package com.nhnacademy.codequestweb.response.point;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class TotalPointAmountResponseDto {
+    Integer totalPoint;
+}
