17c19a2dac0df0bfd5001187f16a8631acd6efeb
jjeonmin <jms959310@gmail.com>
2024-07-10 18:38:51 +0900

    feat: 회원 포인트 뷰 추가

--- src/main/java/com/nhnacademy/codequestweb/client/point/PointUsageClient.java
+++ src/main/java/com/nhnacademy/codequestweb/client/point/PointUsageClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.nhnacademy.codequestweb.response.point.PointUsageMyPageResponseDto;
 
-@FeignClient(name = "clientCoupon", url = "http://localhost:8001")
+@FeignClient(name = "pointUsage", url = "http://localhost:8001")
 
 public interface PointUsageClient {
 
