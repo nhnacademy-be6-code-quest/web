374e5fcbd13cb2c70b3453d00e82c5c07108328e
jjeonmin <jms959310@gmail.com>
2024-07-11 10:29:23 +0900

    feat: 관리자 컨트롤러 추가

---
+++ src/main/java/com/nhnacademy/codequestweb/controller/adminPage/AdminPageController.java
@@ -0,0 +1,11 @@
+package com.nhnacademy.codequestweb.controller.adminPage;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class AdminPageController {
+
+}
