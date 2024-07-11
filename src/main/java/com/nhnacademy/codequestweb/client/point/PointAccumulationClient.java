52585df196b9ee6f85f76571b1f70188cb72215b
jjeonmin <jms959310@gmail.com>
2024-07-11 20:21:49 +0900

    사이드바 수정

--- src/main/java/com/nhnacademy/codequestweb/client/point/PointAccumulationClient.java
+++ src/main/java/com/nhnacademy/codequestweb/client/point/PointAccumulationClient.java
@@ -2,11 +2,14 @@ package com.nhnacademy.codequestweb.client.point;
 
 import com.nhnacademy.codequestweb.response.point.PointAccumulationAdminPageResponseDto;
 import com.nhnacademy.codequestweb.response.point.PointAccumulationMyPageResponseDto;
+import jakarta.servlet.annotation.HttpConstraint;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -20,4 +23,7 @@ public interface PointAccumulationClient {
     @GetMapping("/api/point/adminPage/reward")
     ResponseEntity<Page<PointAccumulationAdminPageResponseDto>> findUserPoint(@RequestHeader
     HttpHeaders headers, @RequestParam int page, @RequestParam int size);
+
+    @DeleteMapping("/api/point/adminPage/delete/{pointAccumulationHistoryId}")
+    ResponseEntity<String> deleteUserPoint(@RequestHeader HttpHeaders headers, @PathVariable long pointAccumulationHistoryId);
 }
