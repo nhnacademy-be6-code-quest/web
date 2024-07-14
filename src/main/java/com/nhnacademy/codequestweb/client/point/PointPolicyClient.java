878e2afb9c2903fb1de4a00865f902e455fbad1c
jjeonmin <jms959310@gmail.com>
2024-07-14 17:49:54 +0900

    feat: 포인트 정책 수정 api추가

--- src/main/java/com/nhnacademy/codequestweb/client/point/PointPolicyClient.java
+++ src/main/java/com/nhnacademy/codequestweb/client/point/PointPolicyClient.java
@@ -1,4 +1,5 @@
 package com.nhnacademy.codequestweb.client.point;
+import com.nhnacademy.codequestweb.request.point.PointPolicyActiveRequestDto;
 import com.nhnacademy.codequestweb.request.point.PointPolicyRegisterRequestDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.data.domain.Page;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,6 +25,6 @@ public interface PointPolicyClient {
     ResponseEntity<Page<PointPolicyAdminListResponseDto>> findAllPointPolicy(@RequestHeader
         HttpHeaders headers, @RequestParam int page, @RequestParam int size);
 
-    @DeleteMapping("/api/point/policy/{pointPolicyId}")
-    ResponseEntity<String> deletePointPolicy(@RequestHeader HttpHeaders headers, @PathVariable long pointPolicyId);
+    @PutMapping("/api/point/policy/active")
+    ResponseEntity<String> pointPolicyActive(@RequestBody PointPolicyActiveRequestDto pointPolicyActiveRequestDto);
 }
