e9982a94a66f1f06f07b79847436cd004b73e4f2
bakhuiwon326 <powerup326@naver.com>
2024-07-22 02:21:38 +0900

    refactor: UserUpdateGradeRequestDto 기본생성자 추가

--- src/main/java/com/nhnacademy/codequestweb/request/payment/UserUpdateGradeRequestDto.java
+++ src/main/java/com/nhnacademy/codequestweb/request/payment/UserUpdateGradeRequestDto.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Builder
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Setter
 public class UserUpdateGradeRequestDto {
     private Long clientId;
 
