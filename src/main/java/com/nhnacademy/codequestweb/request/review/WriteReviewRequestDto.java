9ba3a52da8694ea9164307180197befacef473ca
gihwanJang <90659997+gihwanJang@users.noreply.github.com>
2024-07-26 18:22:41 +0900

    feat : logo

--- src/main/java/com/nhnacademy/codequestweb/request/review/WriteReviewRequestDto.java
+++ src/main/java/com/nhnacademy/codequestweb/request/review/WriteReviewRequestDto.java
@@ -1,6 +1,8 @@
 package com.nhnacademy.codequestweb.request.review;
 
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.*;
 
 @Getter
@@ -11,6 +13,8 @@ import lombok.*;
 public class WriteReviewRequestDto {
     private Long productId;
     private Long productOrderDetailId;
+    @Min(1)
+    @Max(5)
     private byte reviewScore;
     private String reviewContent;
 }
