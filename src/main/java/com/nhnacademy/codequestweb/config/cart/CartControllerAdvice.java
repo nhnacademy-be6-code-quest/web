3af10dc4b5d4e377d5d45a40a7bebde987a6eb65
InJunKangW <149602262+InJunKangW@users.noreply.github.com>
2024-08-01 18:52:23 +0900

    fix: 장바구니 정보를 쿠키에 담는 방식 변경 SecretKey 암호화 -> Base64 인코딩
    기존엔 SecretKey를 어플리케이션 레이어에서 각자 생성해서,
    프론트 이중화 시 그 둘의 SecretKey 가 달라서 문제가 발생하였음.

--- src/main/java/com/nhnacademy/codequestweb/config/cart/CartControllerAdvice.java
+++ src/main/java/com/nhnacademy/codequestweb/config/cart/CartControllerAdvice.java
@@ -1,17 +1,16 @@
 package com.nhnacademy.codequestweb.config.cart;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nhnacademy.codequestweb.request.product.cart.CartRequestDto;
 import com.nhnacademy.codequestweb.service.product.CartService;
 import com.nhnacademy.codequestweb.utils.CookieUtils;
-import com.nhnacademy.codequestweb.utils.SecretKeyUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
@@ -24,9 +23,10 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 public class CartControllerAdvice {
     private final ObjectMapper objectMapper;
 
+    private final CartService cartService;
+
     private final List<CartRequestDto> emptyList = new ArrayList<>();
 
-    private final CartService cartService;
 
     private void restoreClientCartList(HttpServletRequest req, HttpServletResponse resp, Model model) throws Exception {
         List<CartRequestDto> cartListOfCookie = cartService.restoreClientCartList(CookieUtils.setHeader(req)).getBody();
@@ -37,24 +37,24 @@ public class CartControllerAdvice {
 
     @ModelAttribute
     public void addAttributes(HttpServletRequest req, HttpServletResponse resp, Model model) throws Exception {
-        String encryptedCart = CookieUtils.getCookieValue(req, "cart");
-        if (encryptedCart != null) {
+        String encodedCart = CookieUtils.getCookieValue(req, "cart");
+        if (encodedCart != null) {
             try {
-                String cartJson = SecretKeyUtils.decrypt(encryptedCart, SecretKeyUtils.getSecretKey());
+                String cartJson = new String(Base64.getDecoder().decode(encodedCart));
                 List<CartRequestDto> cartListOfCookie = objectMapper.readValue(cartJson, new TypeReference<List<CartRequestDto>>() {});
                 model.addAttribute("cart", cartListOfCookie);
-            } catch (IllegalArgumentException | IllegalBlockSizeException | BadPaddingException e) {
+            } catch (IllegalArgumentException | JsonProcessingException e) {
                 log.warn("cookie has been corrupted");
-                CookieUtils.deleteCookieValue(resp, "cart");
                 if (!CookieUtils.isGuest(req)){
                    restoreClientCartList(req, resp, model);
                 }else{
+                    CookieUtils.setCartCookieValue(emptyList, objectMapper, resp);
                     model.addAttribute("cart", emptyList);
                 }
             } catch (Exception e) {
-                CookieUtils.deleteCookieValue(resp, "cart");
+                CookieUtils.setCartCookieValue(emptyList, objectMapper, resp);
                 model.addAttribute("cart", emptyList);
-                log.error("error while decrypting cart", e);
+                log.error("error while decrypting cart, message : {}", e.getMessage());
             }
         } else {
             log.warn("cookie has been deleted");
